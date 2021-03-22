75062391ab2a342ff64959d7a73c50ea47ef89af
Aalishan <aalishan565@gmail.com>
2021-03-22 13:22:21 +0530

    -change implementation

--- src/sorting/QuickSort.java
+++ src/sorting/QuickSort.java
@@ -2,14 +2,14 @@ package sorting;
 
 public class QuickSort {
     public static void main(String[] args) {
-        int array[] = {2,1, 3, 5, 4};
-        sortArray(array, 0, array.length-1);
+        int array[] = {-2, -4, 1, 2, 3, 4, 5, 6, 2, 1, 5, 4};
+        sortArray(array, 0, array.length - 1);
         printArray(array);
     }
 
     private static void sortArray(int[] array, int left, int right) {
         int mid = partition(array, left, right);
-        if (left < mid-1) {
+        if (left < mid - 1) {
             sortArray(array, left, mid - 1);
         }
         if (mid < right) {
@@ -18,19 +18,22 @@ public class QuickSort {
     }
 
     private static int partition(int[] array, int left, int right) {
-        int pivot = array[(left + right)/2];
-        while (array[left] < pivot) {
-            left++;
-        }
-        while (array[right] > pivot) {
-            right--;
-        }
-        if (left <= right) {
-            int temp = array[left];
-            array[left] = array[right];
-            array[right] = temp;
-            left++;
-            right--;
+        int pivot = array[(left + right) / 2];
+
+        while (left <= right) {
+            while (array[left] < pivot) {
+                left++;
+            }
+            while (array[right] > pivot) {
+                right--;
+            }
+            if (left <= right) {
+                int temp = array[left];
+                array[left] = array[right];
+                array[right] = temp;
+                left++;
+                right--;
+            }
         }
         return left;
     }
