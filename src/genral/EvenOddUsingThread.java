20aa03928f42fd09a1b371962bd7fa06cef61ad3
Aalishan <aalishan565@gmail.com>
2021-04-03 13:21:15 +0530

    Optimized solution

--- src/genral/EvenOddUsingThread.java
+++ src/genral/EvenOddUsingThread.java
@@ -2,7 +2,7 @@ package genral;
 
 public class EvenOddUsingThread {
 
-    static int number = 10;
+    static int number = 100;
     static int counter = 1;
 
     public static void main(String[] args) {
@@ -16,49 +16,39 @@ public class EvenOddUsingThread {
     public void printOdd() {
         synchronized (this) {
             while (counter < number) {
-                while (counter % 2 == 0) {
+                if (counter % 2 == 0) {
                     try {
                         wait();
                     } catch (InterruptedException e) {
-
                     }
                 }
                 // Print the number
-                System.out.print(counter + " ");
-
+                System.out.println(counter + " Thread Name: " + Thread.currentThread().getName());
                 // Increment counter
                 counter++;
-
                 // Notify to second thread
                 notify();
             }
-
         }
-
     }
 
     public void printEven() {
         synchronized (this) {
             while (counter < number) {
-                while (counter % 2 == 1) {
+                if (counter % 2 == 1) {
                     try {
                         wait();
                     } catch (InterruptedException ignored) {
-
                     }
                 }
-
                 // Print the number
-                System.out.print(counter + " ");
-
+                System.out.println(counter + " Thread Name: " + Thread.currentThread().getName());
                 // Increment counter
                 counter++;
-
                 // Notify to second thread
                 notify();
             }
         }
     }
-
 
 }
