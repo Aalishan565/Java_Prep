acaaeacd0a29b346b5c91c0860bd3fc21d74e9fb
Aalishan <aalishan565@gmail.com>
2021-04-16 13:20:16 +0530

    -new program

---
+++ src/genral/_10_SwapTwoNumbers.java
@@ -0,0 +1,13 @@
+package genral;
+
+public class _10_SwapTwoNumbers {
+    public static void main(String[] args) {
+        int number1 = 5;
+        int number2 = 6;
+        number1=number1+number2;
+        number2=number1-number2;
+        number1=number1-number2;
+        System.out.println("Number1 "+number1);
+        System.out.println("Number2 "+number2);
+    }
+}
