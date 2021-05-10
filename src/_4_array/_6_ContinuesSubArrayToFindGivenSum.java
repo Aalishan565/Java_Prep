b64d47654446209a08bf373b1f4913568fcedca6
Aalishan <aalishan565@gmail.com>
2021-05-10 18:34:00 +0530

    -code optimized

--- src/_4_array/_6_ContinuesSubArrayToFindGivenSum.java
+++ src/_4_array/_6_ContinuesSubArrayToFindGivenSum.java
@@ -3,24 +3,23 @@ package _4_array;
 public class _6_ContinuesSubArrayToFindGivenSum {
     public static void main(String args[]) {
         int arr[] = {1, 2, -1, 4, 2, 3, 10};
-        subArraySum(arr, arr.length - 1, 15);
+        subArraySum(arr, 15);
     }
 
-    private static void subArraySum(int[] arr, int length, int sum) {
-        int i;
+    private static void subArraySum(int[] arr, int sum) {
         int start = 0;
         int currentSum = 0;
-        for (i = 0; i <= length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             currentSum = currentSum + arr[i];
             while (currentSum > sum) {
                 currentSum = currentSum - arr[start];
                 start++;
             }
             if (currentSum == sum) {
-                System.out.println("Sub _4_array found from index :" + start + " to " + i);
+                System.out.println("Sub array found from index :" + start + " to " + i);
                 return;
             }
         }
-        System.out.println("No sum found int given _4_array.");
+        System.out.println("No sum found in the given array.");
     }
 }
