a09e81871e02fd8ad37b75679c23ae4a30dd2009
Aalishan <aalishan565@gmail.com>
2021-05-13 20:37:25 +0530

    -code optimization

---
+++ src/_3_string_problems/_10_LongestCommonPrefixInStringArray.java
@@ -0,0 +1,40 @@
+package _3_string_problems;
+
+import java.util.Arrays;
+
+public class _10_LongestCommonPrefixInStringArray {
+
+    public static void main(String[] args) {
+        String[] arr ={"cloth", "clove","clock","clob"};
+        int size = arr.length;
+        // The longest common prefix of an empty array is "".
+        if (size == 0){
+            System.out.println("Longest common prefix: ");
+        }
+        // The longest common prefix of an array containing
+        // only one element is that element itself.
+        else if (size == 1){
+            System.out.println("Longest common prefix: " + arr[0]);
+        }
+        else{
+            // Sort the array
+            Arrays.sort(arr);
+            int length = arr[0].length();
+            StringBuilder res = new StringBuilder();
+            // Comapre the first and the last strings character
+            // by character.
+            for(int i = 0; i < length; i++){
+                // If the characters match, append the character to the result.
+                if(arr[0].charAt(i) == arr[size - 1].charAt(i)){
+                    res.append(arr[0].charAt(i));
+                }
+                // Else, stop the comparison.
+                else{
+                    break;
+                }
+            }
+            String result = res.toString();
+            System.out.println("Longest common prefix: " + result);
+        }
+    }
+}
