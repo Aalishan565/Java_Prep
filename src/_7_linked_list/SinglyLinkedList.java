1e51477a4b8370807414e96feefc58062518dbd8
Aalishan <aalishan565@gmail.com>
2021-05-23 23:19:31 +0530

    -added new method to merge 2 linked list

--- src/_7_linked_list/SinglyLinkedList.java
+++ src/_7_linked_list/SinglyLinkedList.java
@@ -95,41 +95,80 @@ public class SinglyLinkedList {
         // reverse printing linked list
         // printFromEnd(head);
 
-        ListNode he = segrigateListEvenOdd(head);
-        printLinkedList(he);
+        // ListNode he = segrigateListEvenOdd(head);
+        // printLinkedList(he);
     }
 
-    private static ListNode segrigateListEvenOdd(ListNode node) {
-        if (node == null) {
-            return null;
+    public static void printLinkedList(ListNode head) {
+        ListNode current = head;
+        while (current != null) {
+            System.out.println(current.data);
+            current = current.next;
         }
-        ListNode odd = node;
-        ListNode even = odd.next;
-        ListNode evenFirst = even;
+    }
 
-        while (true) {
-            // If there are no more nodes,
-            // then connect first node of even
-            // list to the last node of odd list
-            if (odd == null || even == null || even.next == null) {
-                odd.next = evenFirst;
-                break;
-            }
-            // Connecting odd nodes
-            odd.next = even.next;
-            odd = even.next;
-            // If there are NO more even nodes
-            // after current odd.
-            if (odd.next == null) {
-                even.next = null;
-                odd.next = evenFirst;
-                break;
-            }
-            // Connecting even nodes
-            even.next = odd.next;
-            even = odd.next;
+    private static int getLinkedListLength(ListNode head) {
+        int count = 0;
+        ListNode current = head;
+        while (current != null) {
+            count++;
+            current = current.next;
         }
-        return node;
+        return count;
+    }
+
+    private static ListNode insertElementAtBeginning(ListNode head, int element) {
+        ListNode newNode = new ListNode(element);
+        newNode.next = head;
+        head = newNode;
+        return head;
+    }
+
+    private static ListNode insertAtLast(ListNode head, int element) {
+        ListNode current = head;
+        ListNode newElement = new ListNode(element);
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = newElement;
+        return head;
+    }
+
+    private static void insertNodeAfterGivenNode(ListNode previous, int i) {
+        ListNode newNode = new ListNode(i);
+        newNode.next = previous.next;
+        previous.next = newNode;
+    }
+
+    private static void insertNodeAtGivenPosition(ListNode head, int position, int data) {
+        ListNode newNode = new ListNode(data);
+        int count = 1;
+        ListNode previous = head;
+        while (count < position - 1) {
+            count++;
+            previous = previous.next;
+        }
+        ListNode current = previous.next;
+        newNode.next = current;
+        previous.next = newNode;
+    }
+
+    private static ListNode deleteHead(ListNode head) {
+        ListNode temp = head;
+        head = head.next;
+        temp.next = null;
+        return head;
+    }
+
+    private static ListNode deleteLastNode(ListNode head) {
+        ListNode last = head;
+        ListNode previousToLast = null;
+        while (last.next != null) {
+            previousToLast = last;
+            last = last.next;
+        }
+        previousToLast.next = null;
+        return head;
     }
 
     private static void printFromEnd(ListNode head) {
@@ -138,6 +177,97 @@ public class SinglyLinkedList {
         }
         printFromEnd(head.next);
         System.out.println(head.data);
+    }
+
+    private static ListNode reverserList(ListNode head) {
+        ListNode current = head;
+        ListNode nextNode = head;
+        ListNode previous = null;
+        while (current != null) {
+            nextNode = current.next;
+            current.next = previous;
+            previous = current;
+            current = nextNode;
+        }
+        return previous;
+    }
+
+    private static ListNode reverserListUsingRecursion(ListNode node) {
+        if (node == null || node.next == null) {
+            return node;
+        }
+        ListNode reverseNode = reverserListUsingRecursion(node.next);
+        node.next.next = node;
+        node.next = null;
+        return reverseNode;
+    }
+
+    private static ListNode findNthElement(ListNode head, int position) {
+        ListNode fastPointer = head;
+        ListNode slowPointer = head;
+        for (int i = 0; i < position; i++) {
+            fastPointer = fastPointer.next;
+        }
+        while (fastPointer != null) {
+            fastPointer = fastPointer.next;
+            slowPointer = slowPointer.next;
+        }
+        return slowPointer;
+    }
+
+    private static ListNode middleElement(ListNode head) {
+        ListNode fastPointer = head;
+        ListNode slowPointer = head;
+        while (fastPointer != null && fastPointer.next != null) {
+            fastPointer = fastPointer.next.next;
+            slowPointer = slowPointer.next;
+        }
+        return slowPointer;
+    }
+
+    private static boolean isLoopExist(ListNode head) {
+        ListNode fastPointer = head;
+        ListNode slowPointer = head;
+        while (fastPointer != null && fastPointer.next != null) {
+            fastPointer = fastPointer.next.next;
+            slowPointer = slowPointer.next;
+            if (slowPointer == fastPointer) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static ListNode removeDuplicate(ListNode head) {
+        ListNode current = head;
+        while (current != null && current.next != null) {
+            if (current.data == current.next.data) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+        return head;
+    }
+
+    private static ListNode findStartingOfLoopNode(ListNode head, ListNode pointer) {
+        ListNode temp = head;
+        ListNode slowPointer = pointer;
+        while (slowPointer != temp) {
+            temp = temp.next;
+            slowPointer = slowPointer.next;
+        }
+        return temp;
+    }
+
+    private static void removeTheLoopOfLinkedList(ListNode head, ListNode pointer) {
+        ListNode temp = head;
+        ListNode slowPointer = pointer;
+        while (slowPointer.next != temp.next) {
+            temp = temp.next;
+            slowPointer = slowPointer.next;
+        }
+        slowPointer.next = null;
     }
 
     private static ListNode getIntersectionPoint(ListNode first, ListNode second) {
@@ -182,166 +312,64 @@ public class SinglyLinkedList {
         currentNode.next = listNode;
     }
 
-    private static ListNode removeDuplicate(ListNode head) {
-        ListNode current = head;
-        while (current != null && current.next != null) {
-            if (current.data == current.next.data) {
-                current.next = current.next.next;
+    private static ListNode mergeTwoSortedLinkedList(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        }
+        ListNode tempHead;
+        ListNode mergedList = new ListNode(-1);
+        tempHead = mergedList;
+        while (l1 != null && l2 != null) {
+            if (l1.data <= l2.data) {
+                mergedList.next = l1;
+                l1 = l1.next;
             } else {
-                current = current.next;
+                mergedList.next = l2;
+                l2 = l2.next;
             }
+            mergedList = mergedList.next;
         }
-        return head;
+        if (l1 == null) {
+            mergedList.next = l2;
+        } else if (l2 == null) {
+            mergedList.next = l1;
+        }
+        return tempHead.next;
     }
 
-    private static ListNode findNthElement(ListNode head, int position) {
-        ListNode fastPointer = head;
-        ListNode slowPointer = head;
-        for (int i = 0; i < position; i++) {
-            fastPointer = fastPointer.next;
+    private static ListNode segrigateListEvenOdd(ListNode node) {
+        if (node == null) {
+            return null;
         }
-        while (fastPointer != null) {
-            fastPointer = fastPointer.next;
-            slowPointer = slowPointer.next;
-        }
-        return slowPointer;
-    }
+        ListNode odd = node;
+        ListNode even = odd.next;
+        ListNode evenFirst = even;
 
-    private static ListNode reverserList(ListNode head) {
-        ListNode current = head;
-        ListNode nextNode = head;
-        ListNode previous = null;
-        while (current != null) {
-            nextNode = current.next;
-            current.next = previous;
-            previous = current;
-            current = nextNode;
-        }
-        return previous;
-    }
-
-    private static ListNode reverserListUsingRecursion(ListNode node) {
-        if (node == null || node.next == null) {
-            return node;
-        }
-        ListNode reverseNode = reverserListUsingRecursion(node.next);
-        node.next.next = node;
-        node.next = null;
-        return reverseNode;
-    }
-
-    private static ListNode middleElement(ListNode head) {
-        ListNode fastPointer = head;
-        ListNode slowPointer = head;
-        while (fastPointer != null && fastPointer.next != null) {
-            fastPointer = fastPointer.next.next;
-            slowPointer = slowPointer.next;
-        }
-        return slowPointer;
-    }
-
-    private static boolean isLoopExist(ListNode head) {
-        ListNode fastPointer = head;
-        ListNode slowPointer = head;
-        while (fastPointer != null && fastPointer.next != null) {
-            fastPointer = fastPointer.next.next;
-            slowPointer = slowPointer.next;
-            if (slowPointer == fastPointer) {
-                return true;
+        while (true) {
+            // If there are no more nodes,
+            // then connect first node of even
+            // list to the last node of odd list
+            if (odd == null || even == null || even.next == null) {
+                odd.next = evenFirst;
+                break;
             }
+            // Connecting odd nodes
+            odd.next = even.next;
+            odd = even.next;
+            // If there are NO more even nodes
+            // after current odd.
+            if (odd.next == null) {
+                even.next = null;
+                odd.next = evenFirst;
+                break;
+            }
+            // Connecting even nodes
+            even.next = odd.next;
+            even = odd.next;
         }
-        return false;
+        return node;
     }
 
-    private static ListNode findStartingOfLoopNode(ListNode head, ListNode pointer) {
-        ListNode temp = head;
-        ListNode slowPointer = pointer;
-        while (slowPointer != temp) {
-            temp = temp.next;
-            slowPointer = slowPointer.next;
-        }
-        return temp;
-    }
-
-    private static void removeTheLoopOfLinkedList(ListNode head, ListNode pointer) {
-        ListNode temp = head;
-        ListNode slowPointer = pointer;
-        while (slowPointer.next != temp.next) {
-            temp = temp.next;
-            slowPointer = slowPointer.next;
-        }
-        slowPointer.next = null;
-    }
-
-    private static ListNode deleteLastNode(ListNode head) {
-        ListNode last = head;
-        ListNode previousToLast = null;
-        while (last.next != null) {
-            previousToLast = last;
-            last = last.next;
-        }
-        previousToLast.next = null;
-        return head;
-    }
-
-    private static ListNode deleteHead(ListNode head) {
-        ListNode temp = head;
-        head = head.next;
-        temp.next = null;
-        return head;
-    }
-
-    private static void insertNodeAtGivenPosition(ListNode head, int position, int data) {
-        ListNode newNode = new ListNode(data);
-        int count = 1;
-        ListNode previous = head;
-        while (count < position - 1) {
-            count++;
-            previous = previous.next;
-        }
-        ListNode current = previous.next;
-        newNode.next = current;
-        previous.next = newNode;
-    }
-
-    private static void insertNodeAfterGivenNode(ListNode previous, int i) {
-        ListNode newNode = new ListNode(i);
-        newNode.next = previous.next;
-        previous.next = newNode;
-    }
-
-    private static ListNode insertAtLast(ListNode head, int element) {
-        ListNode current = head;
-        ListNode newElement = new ListNode(element);
-        while (current.next != null) {
-            current = current.next;
-        }
-        current.next = newElement;
-        return head;
-    }
-
-    private static ListNode insertElementAtBeginning(ListNode head, int element) {
-        ListNode newNode = new ListNode(element);
-        newNode.next = head;
-        head = newNode;
-        return head;
-    }
-
-    private static int getLinkedListLength(ListNode head) {
-        int count = 0;
-        ListNode current = head;
-        while (current != null) {
-            count++;
-            current = current.next;
-        }
-        return count;
-    }
-
-    public static void printLinkedList(ListNode head) {
-        ListNode current = head;
-        while (current != null) {
-            System.out.println(current.data);
-            current = current.next;
-        }
-    }
 }
