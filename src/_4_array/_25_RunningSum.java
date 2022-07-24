b69c6ac5f42260487751f150acdde40026fa56bc
Aalishan Ansari <aalishan565@gmail.com>
2022-07-24 18:26:23 +0530

    commit for -running sum array problem

---
+++ src/_4_array/_25_RunningSum.java
@@ -0,0 +1,28 @@
+package _4_array;
+
+public class _25_RunningSum {
+
+    public static void main(String[] args) {
+        int array[] = {1, 2, 3, 4, 5};
+        int runningSum[] = calculateRunningSum(array);
+        for (int value : runningSum
+        ) {
+            System.out.println(value);
+        }
+    }
+
+    private static int[] calculateRunningSum(int[] array) {
+        int newArray[] = new int[array.length];
+        int previousSum = array[0];
+        newArray[0] = previousSum;
+
+        for (int i = 0; i < array.length; i++) {
+            if (i > 0) {
+                previousSum = previousSum + array[i];
+                newArray[i] = previousSum;
+            }
+        }
+        return newArray;
+    }
+
+}
