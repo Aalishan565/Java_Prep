1b1e41c3046b4bc5ac860b10dd6d2cf3efe98615
aalishanansari <aalishan@payo.in>
2022-12-19 15:15:24 +0530

    -simplyfy

--- src/_5_sorting/_1_BubbleSort.java
+++ src/_5_sorting/_1_BubbleSort.java
@@ -29,4 +29,24 @@ public class _1_BubbleSort {
         }
     }
 
+/*
+    private static void sortArray(int[] array) {
+        int temp = 0;
+        for (int i = 0; i < array.length; i++) {
+            int flag=0;
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                if (array[j] > array[j + 1]) {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    flag=1;
+                }
+            }
+            if(flag==0){
+                break;
+            }
+        }
+    }
+*/
+
 }
