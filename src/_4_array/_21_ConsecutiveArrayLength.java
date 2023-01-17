3115520b1c884da5c012996197d68f8a2d4e2390
aalishanansari <aalishan@payo.in>
2023-01-17 13:37:05 +0530

    -simplify

--- src/_4_array/_21_ConsecutiveArrayLength.java
+++ src/_4_array/_21_ConsecutiveArrayLength.java
@@ -13,27 +13,21 @@ public class _21_ConsecutiveArrayLength {
     private static void findLengthOfConsecutiveArray(int[] array) {
         Set<Integer> hashSet = new HashSet<>();
         int longestSequence = 0;
-        int startNumber = 0;
-        int endNumber = 0;
         for (int i = 0; i < array.length; i++) {
             hashSet.add(array[i]);
         }
         for (int i = 0; i < hashSet.size(); i++) {
             if (!hashSet.contains(array[i] - 1)) {
                 int number = array[i];
-                startNumber = number;
                 while (hashSet.contains(number)) {
                     number++;
                 }
                 if (longestSequence < number - array[i]) {
                     longestSequence = number - array[i];
-                    endNumber = number - 1;
                 }
             }
         }
         System.out.println("longestSequence: " + longestSequence);
-        System.out.println("start: " + startNumber);
-        System.out.println("end: " + endNumber);
     }
 
 }
