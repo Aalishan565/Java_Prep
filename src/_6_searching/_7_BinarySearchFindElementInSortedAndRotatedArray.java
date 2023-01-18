27cfe2b585c31227f4b5dfc551d3af34e0de66e2
aalishanansari <aalishan@payo.in>
2023-01-18 12:54:38 +0530

    -simplify

---
+++ src/_6_searching/_7_BinarySearchFindElementInSortedAndRotatedArray.java
@@ -0,0 +1,59 @@
+package _6_searching;
+
+public class _7_BinarySearchFindElementInSortedAndRotatedArray {
+
+    public static void main(String[] args) {
+        int array[] = {5, 6, 7, 1, 2, 3, 4};
+        findElementInSortedAndRotatedArray(array, 9);
+    }
+
+    //Find Number in sorted and rotated array
+    private static void findElementInSortedAndRotatedArray(int[] array, int key) {
+        int pivot = findRotationCountInSortedArray(array);
+        int index;
+        if (array[pivot] <= key && key <= array[array.length - 1]) {
+            index = searchElementWithLoop(array, pivot + 1, array.length - 1, key);
+        } else {
+            index = searchElementWithLoop(array, 0, pivot - 1, key);
+        }
+        System.out.println("Element present at index " + index);
+    }
+
+    //Find Number of Times a Sorted array is Rotated
+    private static int findRotationCountInSortedArray(int[] array) {
+        int start = 0;
+        int end = array.length - 1;
+        int pivot = -1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (mid < end && array[mid + 1] < array[mid]) {
+                pivot = mid + 1;
+                return pivot;
+            } else if (mid > start && array[mid - 1] > array[mid]) {
+                pivot = mid;
+                return pivot;
+            } else if (array[start] < array[mid]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return pivot;
+    }
+
+    //Binary Search with loop
+    private static int searchElementWithLoop(int[] array, int start, int end, int key) {
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (array[mid] == key) {
+                return mid;
+            } else if (array[mid] > key) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+}
---
+++ src/_6_searching/_8_BinarySearchAlgo.java
@@ -0,0 +1,27 @@
+package _6_searching;
+
+public class _8_BinarySearchAlgo {
+
+    public static void main(String[] args) {
+        int array[] = {5, 6, 7, 1, 2, 3, 4};
+        //System.out.println(findMissingElementInSortedArray(array));
+    }
+
+    //Binary missing element in logN
+    static int findMissingElementInSortedArray(int array[]) {
+        int startIndex = 0, endIndex = array.length - 1;
+        int mid;
+        while ((endIndex - startIndex) > 1) {
+            mid = (startIndex + endIndex) / 2;
+            if ((array[startIndex] - startIndex) != (array[mid] - mid)) {
+                endIndex = mid;
+            } else if ((array[endIndex] - endIndex) != (array[mid] - mid)) {
+                startIndex = mid;
+            } else {
+                return -1;
+            }
+        }
+        return (array[startIndex] + 1);
+    }
+
+}
