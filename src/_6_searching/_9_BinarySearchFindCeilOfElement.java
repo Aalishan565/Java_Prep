f5f46b72188c36c8738dc3c3c6527cf6e2d82e21
aalishanansari <aalishan@payo.in>
2023-01-18 13:59:20 +0530

    -simplify

--- src/_6_searching/_9_BinarySearchFindCeilOfElement.java
+++ src/_6_searching/_9_BinarySearchFindCeilOfElement.java
@@ -3,8 +3,30 @@ package _6_searching;
 public class _9_BinarySearchFindCeilOfElement {
 
     public static void main(String[] args) {
-        int array[] = {5, 6, 7, 1, 2, 3, 4};
-        //System.out.println(findMissingElementInSortedArray(array));
+        int array[] = {1, 2, 3, 4, 6, 7, 9};
+        int key = 10;
+        int ceilingIndex = ceilingOfElement(array, key);
+        System.out.println("Ceil of number is " + array[ceilingIndex]);
+    }
+
+    //Binary Search with loop
+    private static int ceilingOfElement(int[] array, int key) {
+        if (key > array[array.length - 1]) {
+            return -1;
+        }
+        int start = 0;
+        int end = array.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (array[mid] == key) {
+                return mid;
+            } else if (array[mid] > key) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return start;
     }
 
 }
--- src/_6_searching/_9_BinarySearchFindFloorOfElement.java
+++ src/_6_searching/_9_BinarySearchFindFloorOfElement.java
@@ -3,8 +3,27 @@ package _6_searching;
 public class _9_BinarySearchFindFloorOfElement {
 
     public static void main(String[] args) {
-        int array[] = {5, 6, 7, 1, 2, 3, 4};
-        //System.out.println(findMissingElementInSortedArray(array));
+        int array[] = {1, 2, 3, 4, 6, 7, 9};
+        int key = 4;
+        int floorIndex = floorOfElement(array, key);
+        System.out.println("Floor of number is " + array[floorIndex]);
+    }
+
+    //Binary Search with loop
+    private static int floorOfElement(int[] array, int key) {
+        int start = 0;
+        int end = array.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (array[mid] == key) {
+                return mid;
+            } else if (array[mid] > key) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return end;
     }
 
 }
