ac929b4f9b108fcb03b2fc3ebe9b381d76f9a1c2
aalishanansari <aalishan@payo.in>
2023-01-18 12:34:46 +0530

    -simplify

---
+++ src/_6_searching/BinarySearchWithLoop.java
@@ -0,0 +1,25 @@
+package _6_searching;
+
+public class BinarySearchWithLoop {
+
+    public static void main(String[] args) {
+        int array[] = {1, 2, 3, 4, 6, 8, 9};
+        int index = searchElementWithLoop(array, 0, array.length - 1, 9);
+        System.out.println("Item found in index " + index);
+    }
+
+    //Binary Search with loop
+    private static int searchElementWithLoop(int[] array, int start, int end, int key) {
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (array[mid] == key) {
+                return mid;
+            } else if (array[mid] > key) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
