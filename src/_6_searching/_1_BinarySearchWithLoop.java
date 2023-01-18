74b6e96af0ce4182c29ccd76cc9f30e8b871ef5a
aalishanansari <aalishan@payo.in>
2023-01-18 14:06:42 +0530

    -simplify

--- src/_6_searching/_1_BinarySearchWithLoop.java
+++ src/_6_searching/_1_BinarySearchWithLoop.java
@@ -4,17 +4,19 @@ public class _1_BinarySearchWithLoop {
 
     public static void main(String[] args) {
         int array[] = {1, 2, 3, 4, 6, 8, 9};
-        int index = searchElementWithLoop(array, 0, array.length - 1, 9);
+        int index = searchElementWithLoop(array, 9);
         System.out.println("Item found in index " + index);
     }
 
     //Binary Search with loop
-    private static int searchElementWithLoop(int[] array, int start, int end, int key) {
+    private static int searchElementWithLoop(int[] array, int element) {
+        int start = 0;
+        int end = array.length - 1;
         while (start <= end) {
             int mid = (start + end) / 2;
-            if (array[mid] == key) {
+            if (array[mid] == element) {
                 return mid;
-            } else if (array[mid] > key) {
+            } else if (array[mid] > element) {
                 end = mid - 1;
             } else {
                 start = mid + 1;
