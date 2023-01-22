847c17eccde62df4c814dc678db4a266aefa6880
aalishanansari <aalishan@payo.in>
2023-01-22 14:54:15 +0530

    -simplify

--- src/_7_linked_list/singly_linked_list/_12_IntersectionPointOf2LinkedList.java
+++ src/_7_linked_list/singly_linked_list/_12_IntersectionPointOf2LinkedList.java
@@ -2,26 +2,26 @@ package _7_linked_list.singly_linked_list;
 
 public class _12_IntersectionPointOf2LinkedList {
 
-    public static ListNode getIntersectionPoint(ListNode first, ListNode second) {
-        int l1 = _2_LengthOfLinkedList.getLinkedListLength(first);
-        int l2 = _2_LengthOfLinkedList.getLinkedListLength(second);
+    public static ListNode getIntersectionPoint(ListNode firstHead, ListNode secondHead) {
+        int l1 = _2_LengthOfLinkedList.getLinkedListLength(firstHead);
+        int l2 = _2_LengthOfLinkedList.getLinkedListLength(secondHead);
         int diff;
         if (l1 > l2) {
             diff = l1 - l2;
             for (int i = 0; i < diff; i++) {
-                first = first.next;
+                firstHead = firstHead.next;
             }
         } else {
             diff = l2 - l1;
             for (int i = 0; i < diff; i++) {
-                first = first.next;
+                secondHead = secondHead.next;
             }
         }
-        while (first.next != null && second.next != null) {
-            first = first.next;
-            second = second.next;
-            if (first == second) {
-                return first;
+        while (firstHead.next != null && secondHead.next != null) {
+            firstHead = firstHead.next;
+            secondHead = secondHead.next;
+            if (firstHead == secondHead) {
+                return firstHead;
             }
         }
         return null;
