5fdc6f34a8d5ef7a699c5044af21b4ce5456fcd0
aalishanansari <aalishan@payo.in>
2023-01-29 17:35:29 +0530

    -simplify

---
+++ src/_4_array/_5_0_ArrayMovingZeroAtEnd.java
@@ -0,0 +1,30 @@
+package _4_array;
+
+public class _5_0_ArrayMovingZeroAtEnd {
+
+    public static void main(String[] args) {
+
+        int arr[] = {2, 0, 1, 0, 1, 5, 1, 0, 0, 0};
+        int arr1[] = sortArrayUsingForLoop(arr);
+        for (int i = 0; i < arr1.length; i++) {
+            System.out.println(arr1[i]);
+        }
+    }
+
+    private static int[] sortArrayUsingForLoop(int[] arr) {
+        int j = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0 && arr[j] == 0) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+            if (arr[j] != 0) {
+                j++;
+            }
+
+        }
+        return arr;
+    }
+
+}
