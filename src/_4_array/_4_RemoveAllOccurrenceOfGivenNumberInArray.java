5a24f86af6f03aeec9e177e3d1aabbfd134ba766
aalishanansari <aalishan@payo.in>
2023-02-08 09:38:12 +0530

    -simplify

--- src/_4_array/_4_RemoveAllOccurrenceOfGivenNumberInArray.java
+++ src/_4_array/_4_RemoveAllOccurrenceOfGivenNumberInArray.java
@@ -1,11 +1,15 @@
 package _4_array;
 
+import java.util.Arrays;
+
 public class _4_RemoveAllOccurrenceOfGivenNumberInArray {
 
     public static void main(String[] args) {
         int array[] = {1, 1, 2, 2, 3};
         int index = removeNumber(array, 2);
-        printArray(array, index);
+        int[] finalArray = removeNumberReturnArray(array, 1);
+       // printArray(array, index);
+        printArray(finalArray, finalArray.length);
     }
 
     private static void printArray(int[] array, int index) {
@@ -25,6 +29,16 @@ public class _4_RemoveAllOccurrenceOfGivenNumberInArray {
             i++;
         }
         return j;
+    }
+
+    private static int[] removeNumberReturnArray(int[] array, int number) {
+        int index = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != number) {
+                array[index++] = array[i];
+            }
+        }
+        return Arrays.copyOf(array, index);
     }
 
 }
