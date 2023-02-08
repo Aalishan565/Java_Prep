16722415d39d87f79889aa1a524fa7057ead9149
aalishanansari <aalishan@payo.in>
2023-02-08 08:00:06 +0530

    -simplify

--- src/_3_string_problems/_1_ReverseString.java
+++ src/_3_string_problems/_1_ReverseString.java
@@ -4,9 +4,22 @@ public class _1_ReverseString {
 
     public static void main(String[] args) {
         String input = "aalishan";
+        reverseString(input);
+    }
+
+    private static void printInReverse(String input) {
         for (int i = input.length() - 1; i >= 0; i--) {
             System.out.print(input.charAt(i));
         }
+    }
+
+    private static void reverseString(String input) {
+        String reversed = "";
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed += input.charAt(i);
+
+        }
+        System.out.print(reversed);
     }
 
 }
