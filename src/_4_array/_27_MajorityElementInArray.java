6b58bfb4f0b7eafa5fe9e17cf296468080ca544e
Aalishan Ansari <aalishan.ansari@tesco.com>
2023-09-11 10:01:19 +0530

    -simplify

---
+++ src/_4_array/_27_MajorityElementInArray.java
@@ -0,0 +1,40 @@
+package _4_array;
+
+public class _27_MajorityElementInArray {
+
+    public static void main(String[] args) {
+        int[] nums = {3, 3, 4, 2, 4, 4, 2, 4, 4};
+        try {
+            int majorityElement = findMajorityElement(nums);
+            System.out.println("The majority element is: " + majorityElement);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static int findMajorityElement(int[] nums) {
+        int candidate = nums[0];
+        int count = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (count == 0) {
+                candidate = nums[i];
+                count = 1;
+            } else if (candidate == nums[i]) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        count = 0;
+        for (int num : nums) {
+            if (num == candidate) {
+                count++;
+            }
+        }
+        if (count > nums.length / 2) {
+            return candidate;
+        } else {
+            throw new IllegalArgumentException("No majority element found");
+        }
+    }
+}
